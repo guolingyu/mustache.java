3250419e543de02794b09da2d263f29397c70b0b
Sam Pullara <spullara@twitter.com>
2011-09-29 09:52:22 -0700

    start adding command line options for generation

---
+++ scaffold/src/main/java/com/sampullara/mustache/scaffold/Generator.java
@@ -0,0 +1,15 @@
+package com.sampullara.mustache.scaffold;
+
+/**
+ * Generate scaffolding for various languages
+ */
+public class Generator {
+
+  @Argument
+  private static String language = "java";
+
+  public static void main(String[] args) {
+
+  }
+
+}
